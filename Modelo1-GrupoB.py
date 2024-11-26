c09ac7be6c27aab99dd5398db64de44c1ddb7f10
andrea.pisu <andrea.pisu@alumnos.upm.es>
2024-11-26 17:08:12 +0100

    actualizado con modelo LP

--- Modelo1-GrupoB.py
+++ Modelo1-GrupoB.py
@@ -5,6 +5,7 @@ Created on Tue Nov 26 12:38:53 2024
 """
 import pulp as lp
 import pandas as pd
+import datetime as dt
 
 df_costes = pd.read_excel("241204_costes.xlsx", index_col = 0)
 df_operaciones = pd.read_excel("241204_datos_operaciones_programadas.xlsx", index_col = 0)
@@ -12,25 +13,61 @@ df_operaciones = pd.read_excel("241204_datos_operaciones_programadas.xlsx", inde
 equipos_cardiologia_pediatrica = df_operaciones[df_operaciones["Especialidad quirúrgica"]=="Cardiología Pediátrica"]
 equipos_medicos = equipos_cardiologia_pediatrica.loc[:,'Equipo de Cirugía']
 
-quirofanos = df_costes.index
+quirofanos = df_costes.index.tolist()
+#%%
+n=0
+# diccionario con nodo y lista de op non compatibles
+L = { x:set() for x in equipos_cardiologia_pediatrica.index}
 
-#C = {(i, j): [df_costes.loc[i, j] for i in equipos_cardiologia_pediatrica for j in str(df_costes.index)]}
+for codigo_a, a in equipos_cardiologia_pediatrica.iterrows():
+    incompat = []    
+    #guardar data de la op
+    a_inicio = a['Hora inicio ']
+    a_fin = a['Hora fin']
+    n += 1
 
+    for num in range(n, len(equipos_cardiologia_pediatrica)):        
+        b = equipos_cardiologia_pediatrica.iloc[num,:]
+        #comparar data inicio y fin
+        b_inicio = b['Hora inicio ']
+        b_fin = b['Hora fin']
+        codigo_b = equipos_cardiologia_pediatrica.index[num]
+        
+        if a_inicio <= b_inicio: 
+            if a_fin > b_inicio:  #incompatible
+                L[codigo_a].add(codigo_b)
+                L[codigo_b].add(codigo_a)
+        else:
+            if b_fin > a_inicio:
+                L[codigo_a].add(codigo_b)
+                L[codigo_b].add(codigo_a)
+
+#%%
 # Los costes se encuentran en la matriz de costes y se filtran al llamar al conjunto de cardiologia pediatrica
+codigos_op = equipos_medicos.index
 
 problema = lp.LpProblem("Entrega 3 Modelo 1", lp.LpMinimize)
 
 #Definir las variables
-x = lp.LpVariable.dicts("x", [(i,j) for i in equipos_medicos for j in quirofanos], lowBound = 0, cat = lp.LpBinary)
+x = lp.LpVariable.dicts("x", [(i,j) for i in codigos_op for j in quirofanos], cat = lp.LpBinary)
 
 #Funcion objetivo
-problema += lp.lpSum(df_costes[(i,j)]*x[(i,j)] for i in equipos_medicos for j in quirofanos)
+problema += lp.lpSum(df_costes.loc[j,i]*x[(i,j)] for i in codigos_op for j in quirofanos)
 
 #Restriccion 1
-for i in equipos_medicos:
+for i in codigos_op:
     problema += lp.lpSum(x[(i,j)] for j in quirofanos) >= 1
     
 #Restriccion 2
-for i in equipos_medicos:
+for i in codigos_op:
     for j in quirofanos: 
-        problema += lp.lpSum(x[(h,j)] for h in L) + x[(i,j)] <= 1
+        problema += lp.lpSum(x[(h,j)] for h in codigos_op if h in L[i]  ) + x[(i,j)] <= 1
+
+problema.solve()
+count = 0
+for v in problema.variables():
+    if v.varValue >0:
+        count += 1
+        print("Var: ",v.name," Valor: ",v.varValue )
+print(count)
+
