8fa2bf6733d3785394994c7aee2876a912b42289
marta-edo <marta.edo@alumnos.upm.es>
2024-12-04 17:50:22 +0100

    Update Modelo3_GrupoB.py

--- Modelo3_GrupoB.py
+++ Modelo3_GrupoB.py
@@ -5,7 +5,75 @@ Created on Wed Dec  4 13:34:26 2024
 @author: marta
 """
 
+<<<<<<< HEAD
 
+=======
+import pandas as pd
+import pulp as lp
+from datetime import datetime, timedelta
+
+df_costes = pd.read_excel("241204_costes.xlsx", index_col = 0)
+df_operaciones = pd.read_excel("241204_datos_operaciones_programadas.xlsx", index_col = 0)
+
+#%% NUEVA PLANIFICACIÓN
+
+#variante de modelo 2, con dos parametros más: lista de quirofanos en input + el ultimo q ha salido de la generacion de columnas
+def nueva_planificacion(operaciones, costes, quirofanos_input, ultimo="Quirófano 1"):  #dataframes con operac ordenadas
+    # version de nueva_p donde ya tenemos los quirofanos selecionados (la funcion dual nos devuelve una lista de quirof utilizados) 
+    #objectivo es generar un conjunto de planificacion K factible
+    
+    #hay q comenzar desde el quirofano nuevo tb?
+    val_inicio = quirofanos_input.index(ultimo)  #pasado como input
+    quirofanos = quirofanos_input[val_inicio,:]+ quirofanos_input[:,val_inicio]
+    
+    
+    K = { q: [] for q in quirofanos } #vamos a poner las operaciones de cada uno
+    activados = []  #quirofanos activados
+    num_activados = 0
+    for codigo,op in operaciones.iterrows():
+        t_inicio = op["Hora inicio "]
+        t_fin = op["Hora fin"]
+        # asigna operacion al primero quirofano disponible
+        asignado = False
+
+        i=0
+        while i < len(activados) and not asignado:
+            quiro = activados[i]
+            if K[quiro][-1][2] <= t_inicio:
+               #libre
+               asignado = True
+               K[quiro].append( (codigo, t_inicio, t_fin) )
+            else: i+=1
+        
+        if not asignado: #nuevo
+            asignado = True
+            quiro = quirofanos[num_activados]
+            K[quiro] = [ (codigo, t_inicio, t_fin) ]
+            activados.append(quiro)
+            num_activados+=1
+    return K
+
+
+
+#%% GENERACIÓN DE COLUMNAS
+
+#hay q sacar precios sombra de funcion primal
+
+def generacion_columnas(precio_sombra,      )
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+>>>>>>> 5a5892149a96aad7674bf4ecd7aac46cb261f7ff
 import pulp as lp
 import numpy as np
 import pandas as pd
@@ -94,6 +162,7 @@ def maestro(operaciones, planificaciones):
     problema.solve()
 #VARIABLES
 
+<<<<<<< HEAD
 #RESTRICCIONES
 
 #FUNCIÓN
@@ -105,3 +174,13 @@ def maestro(operaciones, planificaciones):
 
 #INDICES
 # y(op): operaciones
+=======
+# Mostrar resultados finales
+num_quirofanos_utilizados = sum([lp.value(y[k]) for k in range(len(y))])
+print(f"Número mínimo de quirófanos necesarios: {num_quirofanos_utilizados}")
+for i in range(len(x)):
+    for k in range(len(x[i])):
+        if lp.value(x[i][k]) == 1:
+            print(f"Operación {i} asignada al quirófano {k}")
+'''
+>>>>>>> 5a5892149a96aad7674bf4ecd7aac46cb261f7ff
